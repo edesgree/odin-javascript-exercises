37f19256a86aec71be05bf983d6256c104e50815
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-07 15:47:59 +0200

    exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    let result = "";
+    
+    for (i = 0; i < number; i++) {
+        result = result + string
+    }
+    if(number<0){
+        result="ERROR";
+    }
+    return result;
 };
-
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
