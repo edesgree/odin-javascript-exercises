7507e7d6dac7fa0044ba84d528741f448bcc92e4
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-07 18:56:47 +0200

    exercise 7 js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  //x °F ≘ (x − 32) ×   5 / 9 °C
+  result = (number - 32) * 5/9;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(number) {
+  //x °C ≘ (x ×  9 / 5 + 32) °F
+  result = (number *  9 / 5 + 32);
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
+console.log(ctof(100));
 // Do not edit below this line
 module.exports = {
   ftoc,
