525a63036684b4c8d663cbfecb53dfcfeb6453ef
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-07 15:54:19 +0200

    exercise 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    const array = string.split("");
+    array.reverse();
+    const result = array.join('');
+    
+    return result
 };
 
 // Do not edit below this line
