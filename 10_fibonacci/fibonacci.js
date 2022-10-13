3f8c131cb5d4be4931154f065fc0b85385e2980b
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-13 12:42:31 +0200

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    // 1, 1, 2, 3, 5, 8,
+    const list = [number];
 
+    if (number > 0) {
+        for (let i = 0; i < number; i++) {
+            if (i === 0 || i === 1) {
+                list[i] = 1;
+            }
+            else {
+                list[i] = (list[i - 1] + list[i - 2]);
+            }
+        }
+        return list[number - 1];
+    }
+    return 'OOPS';
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
